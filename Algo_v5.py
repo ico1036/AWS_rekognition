bcbdc6ddaa4dc75ada1fd0cfd59f1ed14fd431be
jkim <jkim@localhost.localdomain>
2018-08-15 02:23:12 +0900

    delete algoritm_v2

--- Algo_v5.py
+++ Algo_v5.py
@@ -35,12 +35,17 @@ default = list(set(name))
 
 # Search special timestamp
 result=[]
-inx=0
+sw=0
 print(len(x))
 
 for i in range(len(x)):
 	for j in x[i][1]:
 		if j not in default:
+			if sw==0:	
+				result.append(x[i-2:i])
+				result.append(x[i:i+3])
+				sw=1
+			else:
 				result.append(x[i:i+3])
 print(result)
 for i in range(1,len(result)-1):
